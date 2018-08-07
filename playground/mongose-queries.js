9b59986e6cc1303067f39ace6aee10cb46bfa53f
马静怡 <472382917@qq.com>
2018-08-07 14:33:32 -0400

    queries

---
+++ playground/mongose-queries.js
@@ -0,0 +1,38 @@
+const {mongoose} =require('../server/db/mogoose');
+
+const {Todo}= require('../server/models/Todo');
+const {User}= require('../server/models/User');
+
+
+const id ='5b69de2b849db2c072e3f1b7';
+
+
+Todo.find({
+    _id: id,
+}).then((r)=>{
+    console.log("Todos", r)
+});
+
+
+Todo.findOne({
+    _id: id,
+}).then((res)=>{
+    console.log("Todo", res)
+});
+
+Todo.findById(id).then((r)=>{
+    if(!r){
+        return console.log("Id not found")
+    }
+    console.log("Id", r)
+});
+
+User.findById(id).then((user)=>{
+    if(!user){
+        return console.log('not found');
+
+    }
+    console.log(user)
+    },(e)=>{
+    console.log(e);
+})
